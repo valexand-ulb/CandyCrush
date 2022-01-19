f9bd3de2002e9bd72ddb11298479bcacc7a03222
alex <vinovrski.alexandre@ulb.be>
2022-01-19 09:09:02 +0100

    AJout du pdf

--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,7 @@
 #include "gui/Mainwindow.h"
 
 int main(int argc, char *argv[]){
-    /*
-    Matrice m{3};
-
-    std::cout<< m << std::endl;
-
-    std::cout<< "Inversion (0,0) <-> (2,2): " << std::endl;
-    m.swapCases({0,0},{2,2});
-    std::cout<< m << std::endl;
-
-    std::cout<< "Supression (1,1): " << std::endl;
-    m.clearCase({1,1});
-    std::cout<< "Supression (0,0): " << std::endl;
-    m.clearCase({0,0});
-    std::cout<< m << std::endl;
-    std::cout<< "Inversion (1,1) <-> (1,2): " << std::endl;
-    m.swapCases({1,1},{1,2});
-    std::cout << m << std::endl;
-    std::cout<< "Remplissage: " << std::endl;
-    m.fillVoid();
-    std::cout<< "Supression (2,0): " << std::endl;
-    m.clearCase({2,0});
-    m.fillVoid();
-    std::cout << m << std::endl;
-    */
     auto mat=std::make_shared<Matrice>(9);
-    std::cout << *mat << std::endl;
     MainWindow window{mat};
     window.show(argc, argv);
     return Fl::run();
